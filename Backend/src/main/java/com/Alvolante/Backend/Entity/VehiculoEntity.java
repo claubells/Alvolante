8e70d65ae282956a0ecbdb7bfa5eb276ee643cc2
claubells <claudia.villa@usach.cl>
2024-12-08 23:09:54 -0300

    aparentemente resuletos los errores de números de excepción

--- Backend/src/main/java/com/Alvolante/Backend/Entity/VehiculoEntity.java
+++ Backend/src/main/java/com/Alvolante/Backend/Entity/VehiculoEntity.java
@@ -2,8 +2,11 @@ package com.Alvolante.Backend.Entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import lombok.Getter;
+
 import java.util.Date;
 
+@Getter
 @Entity
 @Table(name = "vehiculo")
 
@@ -57,69 +60,6 @@ public class VehiculoEntity {
         return disponibilidad;
     }
 
-    public long getIdVehiculo() {
-        return idVehiculo;
-    }
-
-    public String getCodigoACRISS() {
-        return codigoACRISS;
-    }
-
-    public String getEstadoVehiculo() {
-        return estadoVehiculo;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public String getPatente() {
-        return patente;
-    }
-
-    public String getNumeroChasis() {
-        return numeroChasis;
-    }
-
-    public String getKilometraje() {
-        return kilometraje;
-    }
-
-    public float getCosto() {
-        return costo;
-    }
-
-    public int getAnio() {
-        return anio;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getCapacidadPasajeros() {
-        return capacidadPasajeros;
-    }
-
-    public boolean isDisponibilidad() {
-        return disponibilidad;
-    }
-
-    public Date getFechaUltimoMantenimiento() {
-        return fechaUltimoMantenimiento;
-    }
-
-    public byte[] getFotoVehiculo() {
-        return fotoVehiculo;
-    }
 }
 
 
