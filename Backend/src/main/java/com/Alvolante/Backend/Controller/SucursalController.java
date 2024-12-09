d96a590288a55514916c74d7ff06cdf50a86ea04
Alvaro <alvaro.munoz.a@usach.cl>
2024-12-09 11:56:27 -0300

    Se agrega clase sucursal y metodos para obtener datos

---
+++ Backend/src/main/java/com/Alvolante/Backend/Controller/SucursalController.java
@@ -0,0 +1,39 @@
+package com.Alvolante.Backend.Controller;
+
+import com.Alvolante.Backend.Entity.SucursalEntity;
+import com.Alvolante.Backend.Entity.VehiculoEntity;
+import com.Alvolante.Backend.Service.SucursalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sucursales")
+public class SucursalController {
+
+    @Autowired
+    private SucursalService sucursalService;
+
+    @GetMapping("/all")
+    public List<SucursalEntity> getAllSucursales(){
+        return sucursalService.getAllSucursales();
+    }
+
+    @GetMapping("/{id}/vehiculos")
+    public ResponseEntity<List<VehiculoEntity>> getVehiculosBySucursal(@PathVariable long id) {
+        List<VehiculoEntity> vehiculos = sucursalService.getVehiculosBySucursal(id);
+        return ResponseEntity.ok(vehiculos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SucursalEntity> getSucursalById(@PathVariable long id) {
+        SucursalEntity sucursal = sucursalService.getSucursalById(id);
+        if (sucursal != null) {
+            return ResponseEntity.ok(sucursal);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
